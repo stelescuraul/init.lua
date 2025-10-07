1d9511bd42127cc4845806504c859d40282585c2
Raul Stelescu <stelescuraul@gmail.com>
2025-10-07 11:01:45 +0200

    chore: remove text from cmp

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -42,6 +42,7 @@ return {
     },
     config = function()
       -- See `:help cmp`
+      -- local lspkind = require 'lspkind'
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       local icons = require 'icons'
@@ -120,12 +121,49 @@ return {
 
             return vim_item
           end,
+          -- format = lspkind.cmp_format {
+          --   mode = 'text_symbol', -- show only symbol annotations
+          --   maxwidth = {
+          --     -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          --     -- can also be a function to dynamically calculate max width such as
+          --     -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+          --     menu = 50, -- leading text (labelDetails)
+          --     abbr = 50, -- actual suggestion item
+          --   },
+          --   ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          --   show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+          --
+          --   -- The function below will be called before any actual modifications from lspkind
+          --   -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          --   before = function(entry, vim_item)
+          --     -- ...
+          --     vim_item.menu = ({
+          --       nvim_lsp = '[LSP]',
+          --       buffer = '[Buffer]',
+          --       luasnip = '[LuaSnip]',
+          --       nvim_lua = '[Lua]',
+          --     })[entry.source.name]
+          --
+          --     return vim_item
+          --   end,
+          -- },
         },
         sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
+          {
+            name = 'nvim_lsp',
+            priority = 1000,
+            entry_filter = function(entry, ctx)
+              -- filter out TEXT type from lsp
+              if entry:get_kind() == 1 then
+                return false
+              end
+
+              return true;
+            end,
+          },
+          { name = 'luasnip', priority = 800 },
           { name = 'path' },
-          { name = 'buffer' },
+          { name = 'buffer', priority = 700 },
         },
         window = {
           documentation = {
