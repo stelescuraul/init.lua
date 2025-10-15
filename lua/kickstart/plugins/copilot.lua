7fcb1b5871e4dd9b8b6da4d6dc886907786840c3
Raul Stelescu <stelescuraul@gmail.com>
2025-10-15 10:40:17 +0200

    chore: replace github copilot with community copilot

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -1,13 +1,30 @@
 return {
+  -- {
+  --   'github/copilot.vim',
+  --   cond = function()
+  --     return not vim.g.vscode
+  --   end,
+  --   config = function()
+  --     vim.g.copilot_no_tab_map = true
+  --     vim.g.copilot_assume_mapped = true
+  --     vim.keymap.set('i', '<M-l>', 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
+  --   end,
+  -- },
+  --
   {
-    'github/copilot.vim',
-    cond = function()
-      return not vim.g.vscode
-    end,
-    config = function()
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-      vim.keymap.set('i', '<M-l>', 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
-    end,
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = '<M-l>',
+        },
+      },
+    },
   },
 }
