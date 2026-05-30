e949a53320f33043e1628b496c7d816fa60ff849
Raul Stelescu <stelescuraul@gmail.com>
2026-05-31 00:34:55 +0200

    fix: enter on complete

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -8,6 +8,8 @@ return {
     return not vim.g.vscode
   end,
   config = function()
-    require('nvim-autopairs').setup {}
+    require('nvim-autopairs').setup {
+      map_cr = false,
+    }
   end,
 }
