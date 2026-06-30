c6d51e324398576303489cb53ee01e69cd26a574
Raul Stelescu <stelescuraul@gmail.com>
2026-07-01 01:21:00 +0200

    chore: remove unused configs

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -11,7 +11,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        eslint_d = { 'eslint_d' },
+        js = { 'eslint_d' },
+        ts = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -9,8 +9,6 @@ require('lazy').setup({
   -- include a plugin definition from file lua/path/name.lua
 
   -- require 'kickstart/plugins/tokyonight',
-  -- require 'kickstart.plugins.aerial',
-  -- require 'kickstart.plugins.typescript-tools',
   { 'stevearc/dressing.nvim', opts = {} },
   {
     'folke/lazydev.nvim',
@@ -43,7 +41,6 @@ require('lazy').setup({
   require 'kickstart.plugins.gitlinker',
 
   require 'kickstart.plugins.github',
-  -- require 'kickstart.plugins.avante',
   require 'kickstart.plugins.sidekick',
   require 'kickstart.plugins.lazygit',
 
